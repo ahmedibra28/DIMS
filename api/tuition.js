9fb253ced4b18481b37d22e6058fd46d47c8e365
Ahmed <ahmaat19@gmail.com>
2021-10-06 09:31:36 +0300

    adding tuition fee api

--- api/tuition.js
+++ api/tuition.js
@@ -2,7 +2,7 @@ import dynamicAPI from './dynamicAPI'
 
 const url = '/api/tuition'
 
-export const getTuitions = async () => await dynamicAPI('get', url, {})
+export const getTuitions = async (obj) => await dynamicAPI('post', url, obj)
 
 export const generateTuition = async (obj) =>
   await dynamicAPI('post', `${url}/generation`, obj)
---
+++ pages/api/tuition/index.js
@@ -0,0 +1,65 @@
+import nc from 'next-connect'
+import dbConnect from '../../../utils/db'
+import Tuition from '../../../models/Tuition'
+import AssignCourse from '../../../models/AssignCourse'
+import Course from '../../../models/Course'
+import { isAuth } from '../../../utils/auth'
+import moment from 'moment'
+
+const handler = nc()
+
+handler.use(isAuth)
+
+handler.post(async (req, res) => {
+  await dbConnect()
+
+  const { courseType, course, shift } = req.body
+  const semester = Number(req.body.semester)
+  let paymentDate = moment(new Date()).format()
+
+  const startOfMonth = moment(paymentDate).clone().startOf('month').format()
+  const endOfMonth = moment(paymentDate).clone().endOf('month').format()
+
+  const tuitionFees = await Tuition.find({
+    courseType,
+    course,
+    shift,
+    semester,
+    isPaid: false,
+    createdAt: { $gte: startOfMonth, $lt: endOfMonth },
+  })
+    .sort({ createdAt: -1 })
+    .populate('courseType', 'name')
+    .populate('course', 'name')
+    .populate('student', ['fullName', 'picture', 'rollNo'])
+
+  if (tuitionFees.length === 0) {
+    return res
+      .status(404)
+      .send('No unpaid students in the semester or shift you selected')
+  } else {
+    return res.status(200).json(tuitionFees)
+  }
+})
+
+handler.put(async (req, res) => {
+  await dbConnect()
+
+  const paymentDate = moment(new Date()).format()
+
+  const tuition = await Tuition.findOne({
+    _id: req.body._id,
+    isPaid: false,
+  })
+
+  if (!tuition) {
+    return res.status(404).send('Payment has not done successfully')
+  } else {
+    tuition.isPaid = true
+    tuition.paymentDate = paymentDate
+    await tuition.save()
+    return res.status(200).json(tuition)
+  }
+})
+
+export default handler
