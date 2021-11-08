8d5fa9009e7fc29a45d0c9643dbf2855147157b8
Ahmed <ahmaat19@gmail.com>
2021-11-08 14:44:17 +0300

    disabling self registerer

--- pages/api/users/register.js
+++ pages/api/users/register.js
@@ -1,37 +1,40 @@
 import nc from 'next-connect'
 import dbConnect from '../../../utils/db'
 import User from '../../../models/User'
-import { generateToken } from '../../../utils/auth'
+// import { generateToken } from '../../../utils/auth'
 
 const handler = nc()
 
 handler.post(async (req, res) => {
   await dbConnect()
 
-  const { name, email, password } = req.body
+  // const { name, email, password } = req.body
+  const { email } = req.body
   const userExist = await User.findOne({ email })
   if (userExist) {
     return res.status(400).send('User already exist')
   }
 
-  const userCreate = await User.create({
-    name,
-    email,
-    password,
-    group: 'user',
-  })
+  return res.status(400).send('Please contact the system administrator!')
 
-  if (userCreate) {
-    res.status(201).json({
-      _id: userCreate._id,
-      name: userCreate.name,
-      email: userCreate.email,
-      group: userCreate.group,
-      token: generateToken(userCreate._id),
-    })
-  } else {
-    return res.status(400).send('Invalid user data')
-  }
+  // const userCreate = await User.create({
+  //   name,
+  //   email,
+  //   password,
+  //   group: 'user',
+  // })
+
+  // if (userCreate) {
+  //   res.status(201).json({
+  //     _id: userCreate._id,
+  //     name: userCreate.name,
+  //     email: userCreate.email,
+  //     group: userCreate.group,
+  //     token: generateToken(userCreate._id),
+  //   })
+  // } else {
+  //   return res.status(400).send('Invalid user data')
+  // }
 })
 
 export default handler
