2d4a140af2fd9524e275ca532c6d77d78f688c9d
Ahmed <ahmaat19@gmail.com>
2021-11-09 11:11:06 +0300

    adding .gitkeep

--- utils/data.js
+++ utils/data.js
@@ -2,7 +2,7 @@ export const routes = () => {
   return [
     {
       isActive: true,
-      menu: 'Normal',
+      menu: 'Hidden',
       path: '/',
       name: 'Home',
     },
@@ -36,6 +36,84 @@ export const routes = () => {
       path: '/profile',
       name: 'Profile',
     },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/course-type',
+      name: 'Course Type',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/course',
+      name: 'Course',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/subject',
+      name: 'Subject',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/instructor',
+      name: 'Instructor',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/student',
+      name: 'Student',
+    },
+    {
+      isActive: true,
+      menu: 'Normal',
+      path: '/attendance',
+      name: 'Attendance',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/notice',
+      name: 'Notice',
+    },
+    {
+      isActive: true,
+      menu: 'Report',
+      path: '/report/attendance-report',
+      name: 'Attendance',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/tuition/generation',
+      name: 'Generation Tuition',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/tuition/tuition',
+      name: 'Tuition Fee',
+    },
+    {
+      isActive: true,
+      menu: 'Report',
+      path: '/report/tuition-report',
+      name: 'Tuition Fee',
+    },
+    {
+      isActive: true,
+      menu: 'Setting',
+      path: '/setting/clearance-card-generator',
+      name: 'Clearance Card Generator',
+    },
+    {
+      isActive: true,
+      menu: 'Normal',
+      path: '/clearance-confirmation',
+      name: 'Clearance Confirmation',
+    },
   ]
 }
 
@@ -45,6 +123,21 @@ export const groups = (ids) => {
       name: 'admin',
       isActive: true,
       route: ids,
+    },
+    {
+      name: 'instructor',
+      isActive: true,
+      route: [],
+    },
+    {
+      name: 'finance',
+      isActive: true,
+      route: [],
+    },
+    {
+      name: 'student',
+      isActive: true,
+      route: [],
     },
   ]
 }
